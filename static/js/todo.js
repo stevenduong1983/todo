fbe458c2bc6e3461b0084630d8cda0d5a3c5a97c
stevenduong1983 <36424596+stevenduong1983@users.noreply.github.com>
2018-06-05 13:26:39 +0700

    TodoPractice

--- static/js/todo.js
+++ static/js/todo.js
@@ -3,7 +3,7 @@ var app=angular.module('toDo',[]);
 app.controller('toDoController',function ($scope) {
     $scope.todoList=[{todoText:'Finish the app'}];
     $scope.todoAdd = function () {
-        $scope.todoList.push({todoText:$scope.todoText});
+        $scope.todoList.push({todoText: $scope.todoInput});
         $scope.todoInput='';
     };
 })
--- todo/settings.py
+++ todo/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/',
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     'static'
-}
+]
+
