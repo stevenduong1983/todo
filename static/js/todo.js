22fef6cbb3ee511549bed4d78d4b8455e128584d
stevenduong1983 <36424596+stevenduong1983@users.noreply.github.com>
2018-06-05 19:48:44 +0700

    TodoPractice

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,9 +1,17 @@
 var app=angular.module('toDo',[]);
 
 app.controller('toDoController',function ($scope) {
-    $scope.todoList=[{todoText:'Finish the app'}];
+    $scope.todoList=[{todoText:'Finish the app',done:false}];
     $scope.todoAdd = function () {
-        $scope.todoList.push({todoText: $scope.todoInput});
+        $scope.todoList.push({todoText: $scope.todoInput,done:false});
         $scope.todoInput='';
     };
+
+    $scope.remove=function() {
+        var oldList=$scope.todoList;
+        $scope.todoList=[];
+        angular.forEach(oldList,function(x){
+            if(!x.done) $scope.todoList.push(x);
+        });
+    }
 })
